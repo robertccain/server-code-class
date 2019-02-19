2584eb2f3aadf5f1fded564d2832aab8deea74b3
Robert Cain <robert.c.cain@gmail.com>
2019-02-19 14:37:49 -0500

    added concurrency

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -44,7 +44,12 @@
 import http.server
 import requests
 from urllib.parse import unquote, parse_qs
-import os 
+import os
+import threading
+from socketserver import ThreadingMixIn
+
+class ThreadHTTPServer(ThreadingMixIn, http.server.HTTPServer):
+    "This is an HTTPServer that supports thread-based concurrency."
 
 memory = {}
 
@@ -152,5 +157,5 @@ class Shortener(http.server.BaseHTTPRequestHandler):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))   # Use PORT if it's there.
     server_address = ('', port)
-    httpd = http.server.HTTPServer(server_address, Shortener)
+    httpd = ThreadHTTPServer(server_address, Shortener)
     httpd.serve_forever()
